e583a4b49634c2af206714e2103bfe17a0440b8a
jessesec2000 <56051254+jessesec2000@users.noreply.github.com>
2021-03-30 00:10:40 -0400

    Add files via upload

--- module11.py
+++ module11.py
@@ -1,3 +1,4 @@
+#ASSIGNMENT 1
 import requests
 from bs4 import BeautifulSoup as bs
 r=requests.get('https://worldhappiness.report/ed/2021/happiness-trust-and-deaths-under-covid-19/')
@@ -8,14 +9,15 @@ h2s = soup.find_all('h2')
 for h2 in h2s:
     print(h2)
 
-#print(soup.prettify())
+#ASSIGNMENT 2
+print(soup.prettify())
 
-# import csv
-# f = open(r'C:\Users\jesse\OneDrive\Documents\PythonCourse\Module11\COVID19ByCounty.csv','rt')
-# myReader = csv.DictReader(f)
-# for row in myReader:
-#     print(row ['COUNTYNAME'])
-# f.close()
+import csv
+f = open(r'C:\Users\jesse\OneDrive\Documents\PythonCourse\Module11\COVID19ByCounty.csv','rt')
+myReader = csv.DictReader(f)
+for row in myReader:
+    print(row ['COUNTYNAME'])
+f.close()
 
 
 
